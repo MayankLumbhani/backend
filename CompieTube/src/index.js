e492eb0c0dc699d422a2d398c32af3d8e1f32a33
Mayank Lumbhani <lumbhani.mayank.cg@gmail.com>
2026-07-06 15:28:09 +0530

    express app creates and Utils code implement

--- CompieTube/src/index.js
+++ CompieTube/src/index.js
@@ -1,6 +1,16 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config(); 
 
-connectDb();
+connectDb()
+
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is runnung on port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!! ", err)
+})
